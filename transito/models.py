549b229912bab1f32ebdf37b1afbd445a8f06f4e
Transito-Leme <projeto.transito.leme@gmail.com>
2023-08-23 10:18:26 -0300

    inicio banco de dados

--- transito/models.py
+++ transito/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Idoso(models.Model):
+    nome = models.CharField(max_length=255)
+    data_nascimeto = models.DateField()
+    telefone = models.CharField(max_length=14)
+    cnh = models.CharField(max_length=20)
+    val_cnh = models.DateField()
+    email = models.EmailField()
+
+
+class Endereco(models.Model):
+    cep = models.PositiveIntegerField()
+    rua = models.CharField(max_length=255)
+    numero = models.PositiveBigIntegerField()
+    Bairro = models.CharField(max_length=255)
+    cidade = models.CharField(max_length=255)
+    estado = models.CharField(max_length=255)
+
+
+class Representante(models.Model):
+    nome = models.CharField(max_length=255)
+    email = models.EmailField()
+    telefone = models.CharField(max_length=20)
+    rg = models.CharField(max_length=20)
+    rg_expedicao = models.DateField()
+    uf = models.CharField(max_length=2)
+
+
+class EnderecoRep(models.Model):
+    cep = models.PositiveIntegerField()
+    rua = models.CharField(max_length=255)
+    numero = models.PositiveBigIntegerField()
+    Bairro = models.CharField(max_length=255)
+    cidade = models.CharField(max_length=255)
+    estado = models.CharField(max_length=255)
